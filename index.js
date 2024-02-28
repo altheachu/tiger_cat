1c0b689fa138d40842f6cc5e8ff39ffa31d0dbfb
cch <aehtla2012@gmail.com>
2024-02-28 22:18:22 +0800

    [IMP] update function done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
+const path = "./files/";
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
@@ -15,22 +16,46 @@ app.listen(port, ()=>{
 })
 
 app.get("/", (req, res)=>{
-  const path = "./files/";
+  renderView(res);
+})
+
+app.post("/save", (req, res)=>{
+  const fileExtension = ".txt";
+  const fileName = `${req.body.fileNo}${fileExtension}`;
+
+  fs.readFile(`${path}${fileName}`, "utf8", function(err,data){
+    if (err) console.log(err);
+    const paragraphs = data.split("$");
+    const title = paragraphs[0] + "$";
+    const fileContent = title + req.body.content;
+    fs.writeFile(`${path}${fileName}`, fileContent, err=>{
+      if (err) throw err;
+      console.log("The file has been saved.");
+    });
+    renderView(res);
+  })
+})
+
+app.post("/delete", (req, res)=>{
+  console.log(req.body);
+})
+
+function renderView(res) {
   let articles = [];
   let tmpArticles = [];
   let length = 0;
+  // 同步，回傳檔案名稱，停止執行其他程式碼，直到整個讀取過程結束。
+  // const files = fs.readdirSync(path);
   // 非同步，一個在讀取過程結束時執行的回傳函數
   fs.readdir(path, (err, files) => {
     files.forEach((file)=>{
       fs.readFile(`${path}${file}`, "utf8", function(err,data){
         if (err) console.log(err);
-        const getTitle = data.split("\r\n");
-        const aTitle = getTitle.shift();
+        const contentArray = data.split("$");
+        const aTitle = contentArray.shift();
         const idx = file.replace('.txt','');
-        let content = '';
-        getTitle.forEach((paragraph)=>{
-          content += paragraph;
-        })
+        let content = contentArray[0];
+        content = content.trimEnd();
         tmpArticles.push({idx: idx, title: aTitle, content: content});
         length++;
         if (length == files.length){
@@ -53,19 +78,4 @@ app.get("/", (req, res)=>{
       setTimeout(()=>{}, 1000);
     });
   });
-  // 同步，回傳檔案名稱，停止執行其他程式碼，直到整個讀取過程結束。
-  // const files = fs.readdirSync(path);
-  
-  // fs.writeFile("test.txt",'test', err=>{
-  //   if (err) throw err;
-  //   console.log("The file has been saved.");
-  // })
-})
-
-app.post("/save", (req, res)=>{
-  console.log(req.body);
-})
-
-app.post("/delete", (req, res)=>{
-  console.log(req.body);
-})
+}
